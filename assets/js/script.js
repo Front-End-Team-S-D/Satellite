c602dc419adb3751925fe7b7d2e4e571e58c508a
Dorsa M <dorsaamhmdi@gmail.com>
2023-10-09 21:45:46 +0330

    update header

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,7 @@ liMenuTop.forEach(function(val){
         })
     })
 })
+
 window.addEventListener('load', function(){
     spanMenuTop3.forEach(function(val){
         val.style.width = '50%'
@@ -26,4 +27,26 @@ window.addEventListener('load', function(){
 })
 
 
-// =============================================================
+// ======================= onload header ============================
+
+let header = document.getElementsByClassName('headerTop')[0]
+let headerText1 = document.getElementsByClassName('header-text1')[0]
+let headerText2 = document.getElementsByClassName('header-text2')[0]
+let _main = document.getElementsByTagName('main')[0]
+_main.addEventListener('scroll', function(){
+    let sctopHeader  = _main.scrollTop
+    let heightHeader = header.clientHeight
+    console.log(heightHeader);
+    let headertextTop = headerText1.computedStyleMap().get('top').value
+    headerText1.style.top = headertextTop + sctopHeader/2 + 'px'
+    console.log(sctopHeader);
+    let sctopHeader2 = ((sctopHeader*100)/heightHeader)
+    console.log(sctopHeader2);
+    if(sctopHeader2 >=  '10'){
+        headerText1.style.opacity = '0'
+    }
+    else{
+        headerText1.style.opacity = '1'
+    }
+
+})

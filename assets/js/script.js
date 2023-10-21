eaa0b49499b12640596cd34a69f76e5d77e4a0d0
saharpgh <saharpourghasemi02@gmail.com>
2023-10-21 20:56:43 +0330

    onload the earthvisual

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,36 +29,79 @@ window.addEventListener('load', function(){
 
 // ======================= onload header ============================
 
-const _main = document.querySelector('.main')
-const headertext1 = document.querySelector('.header-text1')
-const headertext2 = document.querySelector('.header-text2')
-const headertext1bottom = headertext1.computedStyleMap().get('bottom').value
-const headertext2bottom = headertext2.computedStyleMap().get('bottom').value
-const EarthVisual = document.querySelector('.EarthVisual')
-const vidMainEarth = document.querySelector('.mainearth>video')
-_main.addEventListener('scroll', () => {
-    let sctopheader = _main.scrollTop
-    console.log(sctopheader)
+const _main = document.querySelector('.main');
+const headertext1 = document.querySelector('.header-text1');
+const headertext2 = document.querySelector('.header-text2');
+const headertext1bottom = headertext1.computedStyleMap().get('bottom').value;
+const headertext2bottom = headertext2.computedStyleMap().get('bottom').value;
 
-    if (sctopheader >= 500) {
-        headertext1.style.bottom = headertext1bottom + sctopheader/5 + 'px'
-        headertext2.style.bottom = headertext2bottom + sctopheader/10 + 'px'
-        headertext1.style.display = 'none'
-        headertext2.style.opacity = '1'
-        headertext2.style.display = 'flex'
-        if (sctopheader >= 1500) {
-            headertext2.style.bottom = headertext2bottom + sctopheader/5 + 'px'
-            headertext1.style.bottom = headertext1bottom + sctopheader/5 + 'px'
-            headertext2.style.display = 'none'
-            if(sctopheader >= 1600){
-                vidMainEarth.style.width = '100%'
+const EarthVisual = document.querySelector('.EarthVisual');
+const vidMainEarth = document.querySelector('.mainearth>video');
+const MainEarth = document.querySelector('.mainearth');
+const vidMainEarthwidth = vidMainEarth.computedStyleMap().get('width').value;
+const EarthVisualtop = EarthVisual.computedStyleMap().get('top').value;
+const EarthVisualright = EarthVisual.computedStyleMap().get('right').value;
+
+const sparkle = document.querySelector('.sparkle-com');
+const imgsparkle = document.querySelector('.sparkle-com>img');
+const imgsparklewidth = imgsparkle.computedStyleMap().get('width').value;
+
+
+let animationFrameId = null;
+let animationStart = null;
+const animationDuration = 500;
+
+
+_main.addEventListener('scroll', () => {
+    let sctopheader = _main.scrollTop;
+
+    cancelAnimationFrame(animationFrameId);
+
+    animationFrameId = requestAnimationFrame(() => {
+        if (sctopheader >= 500) {
+            headertext1.style.bottom = headertext1bottom + sctopheader / 5 + 'px';
+            headertext2.style.bottom = headertext2bottom + sctopheader / 10 + 'px';
+            headertext1.style.display = 'none';
+            headertext2.style.opacity = '1';
+            headertext2.style.display = 'flex';
+
+            if (sctopheader >= 1500) {
+                headertext2.style.bottom = headertext2bottom + sctopheader / 5 + 'px';
+                headertext1.style.bottom = headertext1bottom + sctopheader / 5 + 'px';
+                headertext2.style.display = 'none';
             }
+
+            if (sctopheader >= 1500 && sctopheader < 4000) {
+                if (!animationStart) {
+                    animationStart = Date.now();
+                }
+
+                const currentTime = Date.now();
+                const progress = Math.min(1, (currentTime - animationStart) / animationDuration);
+
+                const newWidth = vidMainEarthwidth - progress * (sctopheader / 4000) * 100 + '%';
+                const newTop = EarthVisualtop - progress * (sctopheader / 15.5) + '%';
+                const newRight = EarthVisualright - progress * (sctopheader / 6000) + '%';
+                vidMainEarth.style.width = newWidth;
+                EarthVisual.style.top = newTop;
+                EarthVisual.style.right = newRight;
+
+                const newWidthSparkle = imgsparklewidth - progress * (sctopheader / 4000) * 100 + '%';
+                imgsparkle.style.width = newWidthSparkle;
+                // sparkle.style.display = 'none';
+                if (sctopheader >= 3000) {
+                    sparkle.style.opacity = '1';
+                }
+
+            } else {
+                animationStart = null; 
+            }
+        } else {
+            headertext1.style.bottom = headertext1bottom + sctopheader / 10 + 'px';
+            headertext2.style.bottom = headertext2bottom + sctopheader / 10 + 'px';
+            headertext1.style.display = 'flex';
+            headertext2.style.display = 'none';
         }
-    }
-    else {
-        headertext1.style.bottom = headertext1bottom + sctopheader/10 + 'px'
-        headertext2.style.bottom = headertext2bottom + sctopheader/10 + 'px'
-        headertext1.style.display = 'flex'
-        headertext2.style.display = 'none'
-    }   
-})
+    });
+});
+
